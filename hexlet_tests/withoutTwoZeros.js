7d26c2cceccccbd58b3766454c3c06c5ccaf1d8a
kirill gorobets <kirya@live-master.com>
2020-11-13 15:29:19 +0300

    add new file withoutTwoZeros.js

---
+++ hexlet_tests/withoutTwoZeros.js
@@ -0,0 +1,16 @@
+const withoutTwoZeros = (zeros, ones) => {
+  if (zeros > ones + 1) {
+      return 0;
+  };
+  const fact = (num) => {
+      if (num === 0) {
+          return 1;
+      } 
+          return num * fact(num-1);   
+  };
+  const n = ones + 1;
+  const k = zeros;
+  const result = fact(n)/(fact(k)*fact(n-k));
+  return result;
+};
+console.log(withoutTwoZeros(3,1));
