9918c2343a51786135eb845e0809d474540e8115
kirill gorobets <kirya@live-master.com>
2020-11-16 14:17:59 +0300

    change file areBracetsBalanced.js

--- hexlet_tests/areBracetsBalanced.js
+++ hexlet_tests/areBracetsBalanced.js
@@ -2,18 +2,20 @@ const areBracetsBalsnced = (str) => {
   if (str[0] !== "(" && str[str.length - 1] !== ")") {
     return false;
   }
-  let cnt1 = 0;
-  let cnt2 = 0;
+  let acc = 0;
   for (let i = 0; i < str.length; i += 1) {
-    if (str[i] === "(") {
-      cnt1 += 1;
-    } else if (str[i] === ")") {
-      cnt2 += 1;
-    }
-  }
-  if (cnt1 === cnt2) {
-    return true;
-  }
+      if (str[i] === '(') {
+          acc += 1
+      } else if (str[i] === ')') {
+          acc -= 1
+      } 
+      if (acc < 0) {
+          return false;
+      }
+  } 
+  if (acc === 0) {
+      return true;
+  } 
   return false;
 };
 export default areBracetsBalsnced;
