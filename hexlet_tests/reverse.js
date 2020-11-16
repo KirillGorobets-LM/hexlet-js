1d251751a575af2db7a490bc8b4291b0f57f0bd0
kirill gorobets <kirya@live-master.com>
2020-11-16 11:31:57 +0300

    add file reverse.js

---
+++ hexlet_tests/reverse.js
@@ -0,0 +1,8 @@
+const reverse = (str) => {
+    let result = str[str.length - 1];
+   if (str.length > 1) {
+       result += reverse(str.slice(0, str.length - 1))    
+   }
+   return result;
+};
+export default reverse;
